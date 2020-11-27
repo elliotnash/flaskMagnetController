74ec788c665f06da4f3cb079d6bc1878712301a7
Elliot <elliotnash@gmail.com>
2020-11-27 11:10:36 -0800

    update java script to properly set css checkboxes

--- static/js/post.js
+++ static/js/post.js
@@ -14,10 +14,29 @@ $(document).ready(function() {
             //update em state on client
             if ('em_on' in msg.update){
                 em_state = msg.update.em_on
-                if (em_state) {
-                    document.getElementById('toggle_em').innerHTML = '<input type="submit" value="Toggle Electromagnet (on)">'
+                //fetch the checkbox
+                let elm = document.getElementById('em_check')
+                //if check box is equal to the current server state then do nothing
+                // else fire the checkbox change event
+                if (elm.checked === em_state){
+                    console.log('received data from server matches local data');
                 } else {
-                    document.getElementById('toggle_em').innerHTML = '<input type="submit" value="Toggle Electromagnet (off)">'
+                    console.log('received update from server, updating')
+                    elm.checked = em_state
+                    elm.dispatchEvent(new Event('change'));
+                }
+
+                //update the status text to the bool sent back
+                if (em_state) {
+                    //get status indicator and set text to on
+                    document.getElementById("em_status").innerText = "status: on"
+                    //get sibling of the check box which is the label and set that text
+                    document.getElementById('em_check').nextElementSibling.innerHTML = 'Turn off'
+                } else {
+                    //get status indicator and set text to on
+                    document.getElementById("em_status").innerText = "status: off"
+                    //get sibling of the check box which is the label and set that text
+                    document.getElementById('em_check').nextElementSibling.innerHTML = 'Turn on'
                 }
             }
         }
@@ -29,6 +48,12 @@ $(document).ready(function() {
         socket.emit('my_event', {data: $('#emit_data').val()});
         return false;
     });*/
+
+    //listener for checkbox, when clicked sends data back to server
+    $('input#em_check').click(function (event) {
+        let elm = document.getElementById('em_check')
+        socket.emit('toggle_em', {data: elm.checked});
+    });
 
     $('form#toggle_em').submit(function(event) {
         em_state = !em_state;
